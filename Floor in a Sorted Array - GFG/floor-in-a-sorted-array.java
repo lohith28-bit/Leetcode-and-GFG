34240e20165cbb5bc7b6103433154aa798b6e85b
Lohith V P <66241468+lohith28-bit@users.noreply.github.com>
2022-11-18 16:29:35 +0530

    Added solution - LeetHub

---
+++ Floor in a Sorted Array - GFG/floor-in-a-sorted-array.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Searching
+{
+    public static void main (String[] args)throws IOException {
+
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+           String a[] = read.readLine().trim().split("\\s+");
+           int n = Integer.parseInt(a[0]);
+           long x = Long.parseLong(a[(int)1]);
+           String st[] = read.readLine().trim().split("\\s+");
+        
+           long arr[] = new long[n];
+           for(int i = 0; i < n; i++)
+           {
+               arr[i] = Long.parseLong(st[i]);
+           }
+           
+           System.out.println(new Solution().findFloor(arr, n, x));
+          
+        }
+    }
+    
+}
+// } Driver Code Ends
+
+
+class Solution{
+    
+    // Function to find floor of x
+    // arr: input array
+    // n is the size of array
+    static int findFloor(long arr[], int n, long x)
+    {
+        int strt = 0 , end = n-1;
+        if(x < arr[0]) return -1;
+        while(strt <= end){
+            int mid = strt + (end-strt)/2;
+            if(arr[mid] == x){
+                if(mid == 0) return -1;
+                else return mid;
+            }else if(arr[mid] < x) strt = mid+1;
+            else end = mid-1;
+        }
+        return end;
+    }
+}
