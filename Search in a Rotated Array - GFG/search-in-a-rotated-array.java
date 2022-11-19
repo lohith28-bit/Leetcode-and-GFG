a03aaf93aa02e1e04bed8dddeb478a9999caa9f9
Lohith V P <66241468+lohith28-bit@users.noreply.github.com>
2022-11-19 09:57:38 +0530

    Added solution - LeetHub

---
+++ Search in a Rotated Array - GFG/search-in-a-rotated-array.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+public class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        
+        int t = Integer.parseInt(in.readLine().trim());
+        while (t-- > 0)
+        {
+            int n = Integer.parseInt(in.readLine().trim());
+            int A[] = new int[n];
+            String s[]=in.readLine().trim().split(" ");
+            for (int i = 0;i < n;i++)
+            {
+                A[i] = Integer.parseInt(s[i]);
+            }
+            int key = Integer.parseInt(in.readLine().trim());
+            
+            out.println(new Solution().search(A, 0, n - 1, key));
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    int search(int arr[], int l, int h, int target)
+    {
+        int low = 0, high = arr.length - 1; //<---step 1
+
+        while (low <= high) { //<--- step 2
+            int mid = (low + high) >> 1; //<----step 3
+            if (arr[mid] == target)
+                return mid; // <---step 4
+
+            if (arr[low] <= arr[mid]) { //<---step 5
+                if (arr[low] <= target && arr[mid] >= target)
+                    high = mid - 1; //<---step 6 
+                else
+                    low = mid + 1; //<---step 7
+            } else { //<---step 7
+                if (arr[mid] <= target && target <= arr[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1; 
+    }
+}
